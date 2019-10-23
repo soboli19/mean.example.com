0373f828000466b5376492e4e3fab3dfc590a9b3
ILIYA SOBOLEV <soboli@att.net>
2019-10-22 19:51:06 -0500

    Add a user creation form

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -1,4 +1,5 @@
 var usersApp = (function() {
+
     function viewUsers(){
 
         let uri = `${window.location.origin}/api/users`;
@@ -12,54 +13,109 @@ var usersApp = (function() {
       
         xhr.send();
       
-       // xhr.onload = function(){
+        // xhr.onload = function(){
+        //   let data = JSON.parse(xhr.response);
+        //   console.log(data);
+        //  }
+        // }
         xhr.onload = function(){
-            let app = document.getElementById('app');
-            let data = JSON.parse(xhr.response);
-            let users = data.users;
-            let table = '';
-            let rows = '';
-          
-            //Loop each user record into it's own HTML table row, each user should
-            //have a link a user view
-            for (let i=0; i<users.length; i++) {
-              rows = rows + `<tr>
-                <td>
-                  <a href="#view-${users[i]['_id']}">${users[i]['last_name']}, ${users[i]['first_name']}</a>
-                </td>
-                <td>${users[i]['username']}</td>
-                <td>${users[i]['email']}</td>
-              </tr>`;
-            }
-
-             //Create a users panel, add a table to the panel, inject the rows into the
-  //table
+    
+        let app = document.getElementById('app');
+        let data = JSON.parse(xhr.response);
+        let users = data.users;
+        let table = '';
+        let rows = '';
+      
+        //Loop each user record into it's own HTML table row, each user should
+        //have a link a user view
+        for (let i=0; i<users.length; i++) {
+          rows = rows + `<tr>
+            <td>
+              <a href="#view-${users[i]['_id']}">${users[i]['last_name']}, ${users[i]['first_name']}</a>
+            </td>
+            <td>${users[i]['username']}</td>
+            <td>${users[i]['email']}</td>
+          </tr>`;
+        }
+      
+        //Create a users panel, add a table to the panel, inject the rows into the
+        //table
         table = `<div class="card">
-        <div class="card-header clearfix">
+          <div class="card-header clearfix">
             <h2 class="h3 float-left">Users</h2>
             <div class="float-right">
-            <a href="#create" class="btn btn-primary">New User</a>
+              <a href="#create" class="btn btn-primary">New User</a>
             </div>
-        </div>
-        <div class="table-responsive">
+          </div>
+          <div class="table-responsive">
             <table class="table table-striped table-hover table-bordered">
-            <thead>
+              <thead>
                 <tr>
-                <td>Name</td>
-                <td>Username</td>
-                <td>Email</td>
+                  <td>Name</td>
+                  <td>Username</td>
+                  <td>Email</td>
                 </tr>
-            </thead>
-            <tbody>${rows}</tbody>
+              </thead>
+              <tbody>${rows}</tbody>
             </table>
-        </div>
+          </div>
         </div>`;
-
-//Append the HTML to the #app
-app.innerHTML = table;
-}
+      
+        //Append the HTML to the #app
+        app.innerHTML = table;
       }
-
+    }
+    
+    function createUser(){
+        var app = document.getElementById('app');
+      
+        var form =  `
+            <div class="card">
+              <div class="card-header clearfix">
+                <h2 class="h3 float-left">Create a New User</h2>
+                <div class="float-right">
+                  <a href="#" class="btn btn-primary">Cancel</a>
+                </div>
+              </div>
+              <div class="card-body">
+                <form id="registrationForm" class="card-body">
+                  <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+      
+                  <div class="row">
+                    <div class="form-group col-md-6">
+                      <label for="first_name">First Name</label>
+                      <input type="text" id="first_name" name="first_name" class="form-control" required>
+                    </div>
+      
+                    <div class="form-group col-md-6">
+                      <label for="last_name">Last Name</label>
+                      <input type="text" id="last_name" name="last_name" class="form-control" required>
+                    </div>
+                  </div>
+      
+                  <div class="row">
+                    <div class="form-group col-md-6">
+                      <label for="username">Username</label>
+                      <input type="text" id="username" name="username" class="form-control" required>
+                    </div>
+      
+                    <div class="form-group col-md-6">
+                      <label for="email">Email</label>
+                      <input type="email" id="email" name="email" class="form-control" required>
+                    </div>
+                  </div>
+      
+                  <div class="text-right">
+                    <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+                  </div>
+                </form>
+              </div>
+            </div>
+        `;
+      
+        app.innerHTML=form;
+      }
+      
     return {
         load: function(){
             let hash = window.location.hash;
@@ -67,9 +123,9 @@ app.innerHTML = table;
           
             switch(hashArray[0]){
               case '#create':
-                console.log('CREATE');
-                break;
-          
+                createUser();
+                break;	    
+                            
               case '#view':
                 console.log('VIEW');
                 break;
@@ -87,12 +143,7 @@ app.innerHTML = table;
                 break;
             }
           }
-              } 
-                   //   alert('LOADED');
-     viewUsers();
-      }
-    }
-  
+        }
   })();
   
   usersApp.load();
